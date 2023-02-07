aca265c7a722669d5dff145aac2c2a08555b7317
Ben <71402472+btims512@users.noreply.github.com>
2023-02-06 20:38:56 -0700

    cleaned up

--- server.js
+++ server.js
@@ -49,64 +49,22 @@ app.get("/search", (req, res) => {
 
 // Random Page //
 app.get("/random", (req, res) => {
-  let endpoint =
-    "https://api.giphy.com/v1/gifs/random?api_key=Sd6Qp7OHdPXtMIUXPvOSsZeBVWSl5xA6";
-  $fetch(endpoint)
-    .then((res) => res.json())
-    .then((data) => {
-      let randomArray = [];
-      randomArray.forEach((data) => {
-        randomArray.push(data.images.fixed_height_downsampled.url);
-      });
-      res.render("random.ejs", { data: randomArray });
-      console.log(data);
-    })
-    .catch((error) => console.error("Error from network: ", error));
+  res.render("random.ejs");
 });
 
 // Categories Page //
 app.get("/categories", (req, res) => {
-  let endpoint = `${baseUrl}v1/gifs/categories?api_key=${key}`;
-  $fetch(endpoint)
-    .then((response) => {
-      if (!response.ok) {
-        throw Error(response.statusText);
-      }
-      return response.json();
-    })
-    .then((data) => {
-      let gifArray = [];
-      data.forEach((element) => {
-        gifArray.push(element.images.fixed_height.url);
-      });
-      res.render("categories.ejs", { data: gifArray }); // rendering the data from the gifArray to my home.ejs file //
-    })
-    .catch((error) => console.error("Error from network: ", error));
+  res.render("categories.ejs");
 });
 
 // About Page //
 app.get("/about", (req, res) => {
-  res.render("about.ejs"); // rendering the data from the gifArray to my home.ejs file //
+  res.render("about.ejs");
 });
 
 // Contact Page //
 app.get("/contact", (req, res) => {
-  let endpoint = `${baseUrl}v1/gifs/categories?api_key=${key}`;
-  $fetch(endpoint)
-    .then((response) => {
-      if (!response.ok) {
-        throw Error(response.statusText);
-      }
-      return response.json(); // return what has been fetched in json format //
-    })
-    .then((data) => {
-      let gifArray = [];
-      data.forEach((element) => {
-        gifArray.push(element.images.fixed_height.url);
-      });
-      res.render("contact.ejs", { data: gifArray }); // rendering the data from the gifArray to my home.ejs file //
-    })
-    .catch((error) => console.error("Error from network: ", error));
+  res.render("contact.ejs");
 });
 
 const port = process.env.PORT || 4000;
